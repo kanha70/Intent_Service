c72510c9f14ac4f70669fdf7cd367a291da84e48
Kanhaiya <kanhappti.kl@gmail.com>
2021-11-17 00:11:57 +0530

    Initial Commit

--- app/src/main/java/com/example/intent_expr/MainActivity.kt
+++ app/src/main/java/com/example/intent_expr/MainActivity.kt
@@ -12,18 +12,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     btnStart.setOnClickListener {
-         Intent(this,MyIntentService::class.java).also {
-             startService(it)
-             tvStatus.text = "Service is Running"
-         }
-     }
+//    btnStart.setOnClickListener {
+//         Intent(this,MyIntentService::class.java).also {
+//             startService(it)
+//             tvStatus.text = "Service is Running"
+//         }
+//     }
+//
+//        btnStop.setOnClickListener {
+//            MyIntentService.stopService()
+//            tvStatus.text = "Service is Stoped"
+//        }
 
-        btnStop.setOnClickListener {
-            MyIntentService.stopService()
-            tvStatus.text = "Service is Stoped"
+        btnStart.setOnClickListener {
+            Intent(this,MyService::class.java).also {
+                startService(it)
+                tvStatus.text = "Service Running"
+            }
         }
 
+        btnStop.setOnClickListener {
+            Intent(this,MyService::class.java).also {
+                stopService(it)
+                tvStatus.text = "Service Stopped"
+            }
+        }
+
+        btnSendData.setOnClickListener {
+            Intent(this,MyService::class.java).also {
+                val dataString = etDataString.text.toString()
+                it.putExtra("EXTRA_DATA",dataString)
+                startService(it)
+            }
+        }
 
 
     }
---
+++ app/src/main/java/com/example/intent_expr/MyService.kt
@@ -0,0 +1,30 @@
+package com.example.intent_expr
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyService : Service() {
+
+    val TAG = "MyService"
+
+    init {
+        Log.d(TAG,"Service is Running...")
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val dataString = intent?.getStringExtra("EXTRA_DATA")
+        dataString?.let {
+            Log.d(TAG,dataString)
+        }
+
+        Thread{
+                        while (true){ }
+        }.start()
+
+        return START_STICKY
+    }
+}
